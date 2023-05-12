96d61eb995542f3c8501775457f9706b032e245c
murklynx <max9571@gmail.com>
2023-05-12 14:28:07 +0530

    [Aman]Hook changed for commit message

---
+++ src/main/java/pl/bookstore/services/LoginService.java
@@ -0,0 +1,15 @@
+package pl.bookstore.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+//  email;
+//  password;
+//  if (email == email){
+//    "access granted"
+//  }
+//  else{
+//    return null
+//  }
+}
